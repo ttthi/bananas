eec97c2e3efab883db8bcee76178956a644981c6
mmjm27 <max.matero@hotmail.com>
2025-03-28 22:16:32 +0200

    added a shellscript

---
+++ update_and_run.sh
@@ -0,0 +1,9 @@
+# A shell script for setting permissions and starting the robot
+cd ~/BananasRobot
+git pull origin main
+
+# Enable communication via the top left USB port (where the arduino nano is connected)
+sudo chmod 666 /dev/ttyUSB0
+
+# Start the robot
+python3 run.py
